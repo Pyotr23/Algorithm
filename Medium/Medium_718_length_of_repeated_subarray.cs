06c86f5f0fe7f3229dc22eef4b6fc04b4c881a06
Pyotr <2chilavert3@mail.ru>
2019-07-16 22:59:34 +0300

    Заглох процесс, надо поспать.

--- Medium/Medium_718_length_of_repeated_subarray.cs
+++ Medium/Medium_718_length_of_repeated_subarray.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Medium
 {
-    class Program
+    class Medium_718_length_of_repeated_subarray
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static int FindLength(int[] A, int[] B)
+        {
+            Dictionary<int, int> dictionaryA = new Dictionary<int, int>();
+            int maxLength = 0;
+            int currentLength = 0;
+            int indexInA = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                dictionaryA.Add(A[i], i);
+            }
+            for (int i = 0; i < B.Length; i++)
+            {                
+                if (!dictionaryA.ContainsKey(B[i]))
+                {
+                    currentLength = 0;
+                    continue;
+                }      
+                indexInA = 
+                if (currentLength == 0)
+                {
+                    currentLength++;
+                    continue;
+                }
+
+            }
         }
     }
 }

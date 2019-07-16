adf8dd650d346a21558651019d532f67d3363fdc
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-07-16 18:03:46 +0300

    Начал думать и прервали.

--- easy_1078_occurrences_after_bigram/easy_1078_occurrences_after_bigram.cs
+++ easy_1078_occurrences_after_bigram/easy_1078_occurrences_after_bigram.cs
@@ -6,7 +6,17 @@ namespace Easy
     {
         static void Main(string[] args)
         {
+            string inputString = "we will we will rock you";
+            FindOcurrences(inputString, "we", "will");
             Console.WriteLine("Hello World!");
+        }
+
+        private static string[] FindOcurrences(string text, string first, string second)
+        {
+            string[] array = text.Split($"{first} {second}");
+            foreach (var x in array)
+                Console.WriteLine(x);
+            return new string[] { "" };
         }
     }
 }

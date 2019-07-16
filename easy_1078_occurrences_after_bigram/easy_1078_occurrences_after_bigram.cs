b691e4e29a0ad6fee27e04c47dc23438fbbfd121
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-07-16 14:16:47 +0300

    Initialization.

---
+++ easy_1078_occurrences_after_bigram/easy_1078_occurrences_after_bigram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Easy
+{
+    class Easy_1078_occurrences_after_bigram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

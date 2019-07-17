e0aee05a32ba36071c8e8bf4c3804c6fc9393d88
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-07-17 10:01:46 +0300

    Прямой перебор работает.

--- Medium/Medium_718_length_of_repeated_subarray.cs
+++ Medium/Medium_718_length_of_repeated_subarray.cs
@@ -7,9 +7,13 @@ namespace Medium
     {
         static void Main(string[] args)
         {
-            int[] firstArray = new int[] { 1, 2, 3, 2, 1 };
-
-            Console.WriteLine("Hello World!");
+            //int[] firstArray = new int[] { 1, 2, 3, 2, 1 };
+            //int[] secondArray = new int[] { 3, 2, 1, 4, 7 };
+            int[] firstArray = new int[] { 3, 2, 4, 2, 5, 8 };
+            int[] secondArray = new int[] { 8, 5, 2, 5, 8 };
+            int answear = FindLength(firstArray, secondArray);
+            Console.WriteLine(answear);
+            Console.ReadKey();
         }
 
         static int FindLength(int[] A, int[] B)
@@ -20,8 +24,13 @@ namespace Medium
                 for (int j = 0; j < B.Length; j++)
                 {
                     int currentLength = 0;
-                    while (A[i + currentLength] == B[i + currentLength])
+                    while (((i + currentLength) < A.Length) 
+                        && ((j + currentLength) < B.Length)
+                        && (A[i + currentLength] == B[j + currentLength]))
+                    {
+                        currentLength++;
                         answear = Math.Max(answear, currentLength);
+                    }                        
                 }
             }
             return answear;

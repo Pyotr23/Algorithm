1e9dc3764a62528336b8679c521b10d79b28df0d
Pyotr <2chilavert3@mail.ru>
2019-07-20 00:40:16 +0300

    Добавлен вариант хорошего решения методом динамического программирования.

--- Medium/Medium_718_length_of_repeated_subarray.cs
+++ Medium/Medium_718_length_of_repeated_subarray.cs
@@ -43,6 +43,25 @@ namespace Medium
 
         static int FindLength(int[] A, int[] B)
         {
+            int ans = 0;
+            int[,] memo = new int[A.Length + 1, B.Length + 1];
+            for (int i = A.Length - 1; i >= 0; --i)
+            {
+                for (int j = B.Length - 1; j >= 0; --j)
+                {
+                    if (A[i] == B[j])
+                    {
+                        memo[i, j] = memo[i + 1, j + 1] + 1;
+                        if (ans < memo[i, j])
+                            ans = memo[i, j];
+                    }
+                }
+            }
+            return ans;
+        }
+
+        static int FindLengthBinarySearch(int[] A, int[] B)
+        {
             int lo = 0;
             int hi = Math.Min(A.Length, B.Length) + 1;
             while (lo < hi)

bc967340c4fad49a5b9a3c6dd5b60b66f024402e
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-07-30 14:38:15 +0300

    Добавил решение с бинарным поиском.

--- Easy/Easy_852_peak_index_in_a _mountain_array.cs
+++ Easy/Easy_852_peak_index_in_a _mountain_array.cs
@@ -8,12 +8,38 @@ namespace Easy
     {
         static void Main(string[] args)
         {
-            int[] array = { 0, 2, 1, 0 };
+            int[] array = { 3, 4, 5, 1 };
             Console.WriteLine(PeakIndexInMountainArray(array));
             Console.ReadKey();
         }
 
         public static int PeakIndexInMountainArray(int[] A)
+        {            
+            int startIndex = 0;
+            int endIndex = A.Length - 1;
+            int middleIndex = (endIndex - startIndex) / 2;
+            while (true)
+            {
+                if (A[middleIndex] > A[middleIndex - 1])
+                {
+                    if (A[middleIndex] > A[middleIndex + 1])
+                        return middleIndex;
+                    else
+                    {
+                        startIndex = middleIndex;
+                        middleIndex = middleIndex + (endIndex - startIndex) / 2;
+                    }
+
+                }
+                else
+                {
+                    endIndex = middleIndex;
+                    middleIndex = middleIndex - (endIndex - startIndex) / 2;
+                }                                 
+            }            
+        }
+
+        public static int PeakIndexInMountainArrayBrutalForce(int[] A)
         {
             int i = 0;
             for (; i < A.Length; i++)

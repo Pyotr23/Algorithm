258c29d3fc94f0ef7797159fb4f46c83361a9477
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-07-30 12:44:27 +0300

    Создание класса для новой задачи.

---
+++ Easy/Easy_852_peak_index_in_a _mountain_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy
+{
+    class Easy_852_peak_index_in_a__mountain_array
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+
+        public static int PeakIndexInMountainArray(int[] A)
+        {
+
+        }
+    }
+}

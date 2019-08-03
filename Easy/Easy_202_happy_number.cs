b21b0e71f6408f8a1fadb448e3d8d91945ce93c3
Pyotr <2chilavert3@mail.ru>
2019-08-03 20:17:38 +0300

    Подготовка к решению задачи №202.

---
+++ Easy/Easy_202_happy_number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy
+{
+    class Easy_202_happy_number
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+
+        public bool IsHappy(int n)
+        {
+
+        }
+    }
+}

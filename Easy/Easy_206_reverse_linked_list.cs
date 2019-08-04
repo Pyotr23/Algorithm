b4bceb2366e1168b25310430cc32200710f72e3d
Pyotr <2chilavert3@mail.ru>
2019-08-04 10:03:37 +0300

    Поправил объявление связанного списка.

--- Easy/Easy_206_reverse_linked_list.cs
+++ Easy/Easy_206_reverse_linked_list.cs
@@ -6,16 +6,28 @@ namespace Easy
 {
     public class ListNode
     {
-          public int val;
-          public ListNode next;
-          public ListNode(int x) { val = x; }
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
     }
     class Easy_206_reverse_linked_list
     {
         static void Main(string[] args)
         {
-            
+            var listNode = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                        }
+                    }
+                }
+            };
             Console.ReadKey();
         }
-    }
+    }      
 }

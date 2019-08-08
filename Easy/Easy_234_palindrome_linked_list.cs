50b20d8707016b744da8a2bb02c03ce6878d5e4a
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-08-08 16:48:11 +0300

    Подготовка для решения новой задачи.

---
+++ Easy/Easy_234_palindrome_linked_list.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy
+{    
+    class Easy_234_palindrome_linked_list
+    {
+        static void Main(string[] args)
+        {            
+            Console.ReadKey();
+        }
+
+        public bool IsPalindrome(ListNode head)
+        {
+
+        }
+    }
+}

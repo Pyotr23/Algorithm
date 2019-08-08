af5550534a27d2e4d7ae2115068221dff06c013e
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-08-08 14:21:04 +0300

    Добавлен рекурсивный вариант.

--- Easy/Easy_206_reverse_linked_list.cs
+++ Easy/Easy_206_reverse_linked_list.cs
@@ -12,40 +12,54 @@ namespace Easy
     }
     class Easy_206_reverse_linked_list
     {
-        static void Main(string[] args)
-        {
-            var listNode = new ListNode(1)
-            {
-                next = new ListNode(2)
-                {
-                    next = new ListNode(3)
-                    {
-                        next = new ListNode(4)
-                        {
-                            next = new ListNode(5)
-                        }
-                    }
-                }
-            };
-            ListNode list = ReverseList(listNode);
-            Console.WriteLine();
-            PrintList(list);
-            Console.ReadKey();
-        }
+        //static void Main(string[] args)
+        //{
+        //    var listNode = new ListNode(1)
+        //    {
+        //        next = new ListNode(2)
+        //        {
+        //            next = new ListNode(3)
+        //            {
+        //                next = new ListNode(4)
+        //                {
+        //                    next = new ListNode(5)
+        //                }
+        //            }
+        //        }
+        //    };
+        //    ListNode list = ReverseList(listNode);
+        //    Console.WriteLine();
+        //    PrintList(list);
+        //    Console.ReadKey();
+        //}
 
-        public static ListNode ReverseList(ListNode head)
-        {
-            ListNode temp = null;
+        public static ListNode ReverseListIterate(ListNode head)
+        {            
             ListNode reverseList = null;
             while (head != null)
             {
-                temp = head;
+                ListNode temp = head;
                 Console.Write($"{head.val} -> ");
                 head = head.next;
                 temp.next = reverseList;
                 reverseList = temp;                   
             }
             return reverseList;
+        }
+
+        public static ListNode ReverseList(ListNode head)
+        {
+            return ReverseList(head, null);            
+        }
+        public static ListNode ReverseList(ListNode input, ListNode answear)
+        {
+            if (input == null)
+                return answear;
+            ListNode temp = input;
+            input = input.next;
+            temp.next = answear;
+            answear = temp;
+            return ReverseList(input, answear);
         }
 
         static void PrintList(ListNode head)

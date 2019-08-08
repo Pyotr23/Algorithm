ef84de31f97b8a2c56fa19c5e9733d5cc39a93ff
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-08-08 17:27:09 +0300

    Если fastNode == null в конце метода, то slowNode стоит на середине, а если fastNode.next == null, то slowNode.next - это начало списка в обратную сторону.

--- Easy/Easy_234_palindrome_linked_list.cs
+++ Easy/Easy_234_palindrome_linked_list.cs
@@ -7,13 +7,39 @@ namespace Easy
     class Easy_234_palindrome_linked_list
     {
         static void Main(string[] args)
-        {            
+        {
+            var listNode = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(1)
+                        //{
+                        //    next = new ListNode(5)
+                        //}
+                    }
+                }
+            };
+            IsPalindrome(listNode);
             Console.ReadKey();
         }
 
-        public bool IsPalindrome(ListNode head)
+        public static void IsPalindrome(ListNode head)
         {
-
+            ListNode slowNode = null;
+            ListNode fastNode = null;
+            while (!(fastNode == null || fastNode?.next == null) || (slowNode == null))
+            {
+                if (slowNode == null)
+                {
+                    slowNode = head;
+                    fastNode = head.next;
+                    continue;
+                }
+                slowNode = slowNode.next;
+                fastNode = fastNode.next.next;
+            }
         }
     }
 }

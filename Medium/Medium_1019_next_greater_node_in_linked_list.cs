9acb1b84d33d8a01ab412ca0b8a6c6a11ad2d95f
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-08-13 12:56:53 +0300

    Подготовка к решению новой задачи.

---
+++ Medium/Medium_1019_next_greater_node_in_linked_list.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium
+{
+    class Medium_1019_next_greater_node_in_linked_list
+    {
+        static void Main(string[] args)
+        {            
+            Console.ReadKey();
+        }
+
+        public static int[] NextLargerNodes(ListNode head)
+        {
+
+        }
+    }
+}

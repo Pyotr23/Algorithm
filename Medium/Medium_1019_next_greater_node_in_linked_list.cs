3f2c2153a65756261604e96c938a1bae20a5a2f0
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-08-14 14:49:24 +0300

    Добавил метод, возвращающий из списка очередь возрастающих значений.

--- Medium/Medium_1019_next_greater_node_in_linked_list.cs
+++ Medium/Medium_1019_next_greater_node_in_linked_list.cs
@@ -15,32 +15,59 @@ namespace Medium
     {
         static void Main(string[] args)
         {
-            var listNode = new ListNode(9)
+            var listNode = new ListNode(10)
             {
-                next = new ListNode(7)
+                next = new ListNode(8)
                 {
                     next = new ListNode(6)
                     {
-                        next = new ListNode(7)
+                        next = new ListNode(4)
                         {
-                            next = new ListNode(6)
+                            next = new ListNode(2)
                             {
-                                next = new ListNode(9)
-                                //{
-                                //    next = new ListNode(5)
-                                //    {
-                                //        next = new ListNode(1)
-                                //    }
-                                //}
+                                next = new ListNode(3)
+                                {
+                                    next = new ListNode(4)
+                                    {
+                                        next = new ListNode(5)
+                                        {
+                                            next = new ListNode(4)
+                                            {
+                                                next = new ListNode(3)
+                                                {
+                                                    next = new ListNode(2)
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
                 }
             };
-            int[] array = NextLargerNodes(listNode);
-            foreach(var x in array)
-                Console.Write(x);
+            //int[] array = NextLargerNodes(listNode);
+            //foreach(var x in array)
+            //    Console.Write(x);
+            var myStack = GetQueue(listNode);
+            while (myStack.Count != 0)
+            {
+                Console.Write($"{myStack.Dequeue()} ");
+            }
             Console.ReadKey();            
+        }
+
+        private static Queue<int> GetQueue(ListNode lNode)
+        {
+            var tempListNode = lNode;
+            var queue = new Queue<int>();           
+            while (tempListNode.next != null && tempListNode.next.next != null)
+            {
+                if (tempListNode.next.val > tempListNode.val)
+                    queue.Enqueue(tempListNode.next.val);
+                tempListNode = tempListNode.next;
+            }
+            return queue;
         }
 
         private static int[] NextLargerNodes(ListNode head)

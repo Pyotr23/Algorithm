4dda5fa9a65c8677ed8794f28b4cea6b8e9f0866
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-08-16 12:28:27 +0300

    Добавил вспомогательные методы.

--- Medium/Medium_82_remove_duplicates_from_sorted_list_II.cs
+++ Medium/Medium_82_remove_duplicates_from_sorted_list_II.cs
@@ -8,12 +8,64 @@ namespace Medium
     {
         static void Main(string[] args)
         {            
+            var listNode = GetListNode(new int[] { 1, 2, 3, 3, 4, 4, 5 });
+            PrintList(listNode);
+            Console.WriteLine();
+            var reverseListNode = ReverseList(listNode);
+            PrintList(reverseListNode);
             Console.ReadKey();
         }
-    }
 
-    public static ListNode DeleteDuplicates(ListNode head)
-    {
+        //public static ListNode DeleteDuplicates(ListNode head)
+        //{
+        //    var tempCurrentList = head;
+        //    while (head != null)
+        //    {
+        //        if (head.next != null && head.next.val != head.val)
+        //        {
+        //            head = head.next;
+        //        }                
+        //    }
+        //}
 
-    }
+        private static ListNode GetListNode(int[] array)
+        {
+            ListNode listNode = null;
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                ListNode tempListNode = new ListNode(array[i])
+                {
+                    next = listNode
+                };
+                listNode = tempListNode;
+            }
+            return listNode;
+        }
+
+        public static ListNode ReverseList(ListNode head)
+        {
+            return ReverseList(head, null);
+        }
+
+        public static ListNode ReverseList(ListNode input, ListNode answear)
+        {
+            if (input == null)
+                return answear;
+            ListNode temp = input;
+            input = input.next;
+            temp.next = answear;
+            answear = temp;
+            return ReverseList(input, answear);
+        }
+
+        static void PrintList(ListNode head)
+        {
+            var tempListNode = head;
+            while (tempListNode != null)
+            {
+                Console.Write($"{tempListNode.val} -> ");
+                tempListNode = tempListNode.next;
+            }
+        }
+    }    
 }

ca1ba660c56a6a94c2145cff6a081d1931236522
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-08-16 12:09:15 +0300

    Подготовка к решению задачи.

---
+++ Medium/Medium_82_remove_duplicates_from_sorted_list_II.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium
+{
+    class Medium_82_remove_duplicates_from_sorted_list_II
+    {
+        static void Main(string[] args)
+        {            
+            Console.ReadKey();
+        }
+    }
+
+    public static ListNode DeleteDuplicates(ListNode head)
+    {
+
+    }
+}

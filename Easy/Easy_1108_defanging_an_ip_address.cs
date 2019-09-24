28beb7d5e30b6ab70c481b1a9fc42209fc10d14e
Pyotr <2chilavert3@mail.ru>
2019-09-24 23:27:15 +0300

    Скорость - 88%, память - 100%. Краткость - сестра таланта.

--- Easy/Easy_1108_defanging_an_ip_address.cs
+++ Easy/Easy_1108_defanging_an_ip_address.cs
@@ -4,17 +4,18 @@ using System.Text;
 
 namespace Easy
 {
-    class Easy_1108_defanging_an_ip_address.cs
+    class Easy_1108_defanging_an_ip_address
     {
         static void Main(string[] args)
         {
-            
+            string ip = "255.100.50.0";
+            Console.WriteLine(DefangIPaddr(ip));
             Console.ReadKey();
         }
 
         private static string DefangIPaddr(string address)
         {
-           
+            return address.Replace(".", "[.]");
         }
     }
 }

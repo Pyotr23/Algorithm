ab528829e2f36f6e80f558baf4250d355c7e48fc
Pyotr <2chilavert3@mail.ru>
2019-09-24 23:10:20 +0300

    Подготовка к решению.

---
+++ Easy/Eay_1108_Defanging_an_IP_address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy
+{
+    class Eay_1108_Defanging_an_IP_address
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+
+        private static string DefangIPaddr(string address)
+        {
+           
+        }
+    }
+}

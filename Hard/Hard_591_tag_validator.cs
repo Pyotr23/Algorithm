7c1222db50b825deb628f48750a8cd2d7ec95d6c
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2019-09-25 16:15:26 +0300

    Создана ветка для задачи.

---
+++ Hard/Hard_591_tag_validator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hard
+{
+    class Hard_591_tag_validator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

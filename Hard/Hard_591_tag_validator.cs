f1ccac0691eda155e77da1190a4c0b479a7a6269
Pyotr <2chilavert3@mail.ru>
2019-09-25 23:49:49 +0300

    Написана проверка обрамляющего тега.

--- Hard/Hard_591_tag_validator.cs
+++ Hard/Hard_591_tag_validator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Hard
 {
@@ -6,12 +7,37 @@ namespace Hard
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "<DIV>This is the first line <![CDATA[<div>]]></DIV>";
+            IsValid(input);            
+            Console.ReadKey();
         }
 
         public static bool IsValid(string code)
         {
+            string tag = GetTag(code);
+            if (string.IsNullOrEmpty(tag))
+                return false;
+            code = code.Substring(tag.Length);
+            string necessaryEndTag = tag.Insert(1, "/");            
+            if (!code.EndsWith(necessaryEndTag))
+                return false;
+            code = code.Remove(code.Length - necessaryEndTag.Length);
+            Console.WriteLine(code);
+            return true;
+        }
 
+        public static string GetTag(string code)
+        {
+            var regex = new Regex(@"^<[A-Z]{1,9}>");
+            var match = regex.Match(code);
+            return match.Value;
+        }
+
+        public static string GetEndTag(string code, string endTag)
+        {
+            var regex = new Regex(endTag);
+            var match = regex.Match(code);
+            return match.Value;
         }
     }
 }

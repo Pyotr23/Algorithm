03feb7a11f7df4b381fc7926cbc3c4713ec00455
Pyotr <2chilavert3@mail.ru>
2019-09-30 22:38:50 +0300

    Сделал проверку первого обрамляющего тега более лаконичной.

--- Hard/Hard_591_tag_validator.cs
+++ Hard/Hard_591_tag_validator.cs
@@ -7,35 +7,22 @@ namespace Hard
     {
         static void Main(string[] args)
         {
-            string input = "<DIV>This is the first line <![CDATA[<div>]]></DIV>";
-            IsValid(input);            
+            string input = "<DIV>This is the first line <![CDATA[<div>]]></DIV>";            
+            Console.WriteLine(IsValid(input));
             Console.ReadKey();
         }
 
         public static bool IsValid(string code)
         {
-            string tag = GetTag(code);
-            if (string.IsNullOrEmpty(tag))
-                return false;
-            code = code.Substring(tag.Length);
-            string necessaryEndTag = tag.Insert(1, "/");            
-            if (!code.EndsWith(necessaryEndTag))
-                return false;
-            code = code.Remove(code.Length - necessaryEndTag.Length);
-            Console.WriteLine(code);
+            string codeWithoutMainTag = GetCodeWithCheckingTag(code);
+            if (string.IsNullOrEmpty(codeWithoutMainTag))
+                return false;            
             return true;
         }
 
-        public static string GetTag(string code)
-        {
-            var regex = new Regex(@"^<[A-Z]{1,9}>");
-            var match = regex.Match(code);
-            return match.Value;
-        }
-
-        public static string GetEndTag(string code, string endTag)
-        {
-            var regex = new Regex(endTag);
+        public static string GetCodeWithCheckingTag(string code)
+        {            
+            var regex = new Regex(@"(?<=^<(?<tagName>[A-Z]{1,9})>).+(?=</\k<tagName>>)");
             var match = regex.Match(code);
             return match.Value;
         }

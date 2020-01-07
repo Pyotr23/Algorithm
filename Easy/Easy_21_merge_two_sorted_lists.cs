38c5aea9285ad8b938101eeedf2b2d42f799e9a8
Pyotr <2chilavert3@mail.ru>
2020-01-08 01:26:31 +0300

    Самый первый тест пройден.

--- Easy/Easy_21_merge_two_sorted_lists.cs
+++ Easy/Easy_21_merge_two_sorted_lists.cs
@@ -8,18 +8,25 @@ namespace Easy
     {
         static void Main(string[] args)
         {
-            var listNode = new ListNode(1) { next = new ListNode(3) { next = new ListNode(5) } };
-            listNode.PrintListNode();
-
-            var secondList = new ListNode(new List<int> { 1, 1, 2, 3, 4, 4 });
-            secondList.PrintListNode();
-
+            var mergedListNode = MergeTwoLists(new ListNode(new List<int> { 1, 2, 4 }), new ListNode(new List<int> { 1, 3, 4 }));
+            mergedListNode.PrintListNode();
             Console.ReadKey();
         }
 
-        //public ListNode MergeTwoLists(ListNode l1, ListNode l2)
-        //{
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            ListNode answearListNode = new ListNode(l1.val) { next = new ListNode(l2.val) };
+            answearListNode.next.next = MergeTwoInOneLists(l1.next, l2.next);
+            return answearListNode;
+        }
 
-        //}
+        private static ListNode MergeTwoInOneLists(ListNode first, ListNode second)
+        {
+            if (first == null && second == null)
+                return null;
+            var nextNode = new ListNode(first.val) { next = new ListNode(second.val) };
+            nextNode.next.next = MergeTwoInOneLists(first.next, second.next);
+            return nextNode;            
+        }
     }
 }

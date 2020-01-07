1dcfe944f0f1f2a77e9727b073c2aa768515bb8e
Pyotr <2chilavert3@mail.ru>
2020-01-08 01:46:24 +0300

    Лаконичная запись решения, но при входных листах [2] и [1] выдаёт ошибку, а не [1, 2], хотя мой ответ [2, 1] считаю правильным.

--- Easy/Easy_21_merge_two_sorted_lists.cs
+++ Easy/Easy_21_merge_two_sorted_lists.cs
@@ -8,16 +8,39 @@ namespace Easy
     {
         static void Main(string[] args)
         {
-            var mergedListNode = MergeTwoLists(new ListNode(new List<int> { 1, 2, 4 }), new ListNode(new List<int> { 1, 3, 4 }));
+            //var mergedListNode = MergeTwoLists(new ListNode(new List<int> { 1, 2, 4 }), new ListNode(new List<int> { 1, 3, 4 }));
+            var mergedListNode = MergeTwoLists(new ListNode(new List<int> { 2 }), new ListNode(new List<int> { 1, 5, 6 }));
             mergedListNode.PrintListNode();
             Console.ReadKey();
         }
 
         public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
-        {
-            ListNode answearListNode = new ListNode(l1.val) { next = new ListNode(l2.val) };
-            answearListNode.next.next = MergeTwoInOneLists(l1.next, l2.next);
-            return answearListNode;
+        {            
+            if (l1 == null && l2 == null)
+                return null;
+            if (l1 == null)
+                return l2;
+            if (l2 == null)
+                return l1;
+            ListNode nextNode = new ListNode(l1.val) { next = new ListNode(l2.val) };
+            nextNode.next.next = MergeTwoLists(l1.next, l2.next);
+            //if (l1 != null)
+            //{
+            //    nextNode = new ListNode(l1.val);
+            //    if (l2 != null)
+            //    {
+            //        nextNode.next = new ListNode(l2.val);
+            //        nextNode.next.next = MergeTwoLists(l1.next, l2.next);
+            //    }
+            //    else
+            //        nextNode.next = MergeTwoLists(l1.next, null);
+            //}
+            //else
+            //{
+            //    nextNode = new ListNode(l2.val);
+            //    nextNode.next = MergeTwoLists(null, l2.next);
+            //}
+            return nextNode;
         }
 
         private static ListNode MergeTwoInOneLists(ListNode first, ListNode second)

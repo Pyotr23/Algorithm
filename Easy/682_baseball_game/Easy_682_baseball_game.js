f67add0605b5d98689e72023fa5b24e1236ae98a
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2020-10-01 17:16:28 +0300

    Задание решено на JS (время - 83%, место - 26%).

--- Easy/682_baseball_game/Easy_682_baseball_game.js
+++ Easy/682_baseball_game/Easy_682_baseball_game.js
@@ -1,31 +1,25 @@
 ﻿var calPoints = function (ops) {
     let sum = 0;
-    const stack = [];
+    const arr = [];
     let newValue = 0;
     ops.forEach(operation => {
         switch (operation) {
-            case "+":                
-                const lastValue = stack.pop();
-                const preLastValue = stack[stack.length - 1];
-                newValue = lastValue + preLastValue;
-                sum += newValue;
-                stack.push(lastValue);
-                stack.push(newValue);
+            case "+":
+                newValue = arr[arr.length - 1] + arr[arr.length - 2];
                 break;
             case "D":
-                newValue = stack[stack.length - 1] * 2;
-                stack.push(newValue);
-                sum += newValue;
+                newValue = arr[arr.length - 1] * 2;
                 break;
             case "C":
-                sum -= stack.pop();
+                newValue = -arr.pop();
                 break;
             default:
                 newValue = Number(operation);
-                stack.push(newValue);
-                sum += newValue;
                 break;
         }
+        if (operation !== "C")
+            arr.push(newValue);
+        sum += newValue;
     });
     return sum;
 };

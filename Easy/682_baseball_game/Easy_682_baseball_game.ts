9645242069c79cb233289a4ccd77a5d5a6602cbd
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2020-10-02 09:21:00 +0300

    Задание решено на JS (время - 27%, место - 18%).

---
+++ Easy/682_baseball_game/Easy_682_baseball_game.ts
@@ -0,0 +1,25 @@
+﻿var calPoints = function (ops: string[]) {
+    let sum = 0;
+    const arr: string[] = [];
+    let newValue = 0;
+    ops.forEach(operation => {
+        switch (operation) {
+            case "+":
+                newValue = +arr[arr.length - 1] + +arr[arr.length - 2];
+                break;
+            case "D":
+                newValue = +arr[arr.length - 1] * 2;
+                break;
+            case "C":
+                newValue = -Number(arr.pop());
+                break;
+            default:
+                newValue = +operation;
+                break;
+        }
+        if (operation !== "C")
+            arr.push(newValue.toString());
+        sum += newValue;
+    });
+    return sum;
+};

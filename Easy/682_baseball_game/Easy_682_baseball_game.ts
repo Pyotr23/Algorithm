d6520f2de99a2833eb6db72e9ad87e077239481a
Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>
2020-10-05 09:02:05 +0300

    Задание решено на TS (время - 64%, место - 27%).

--- Easy/682_baseball_game/Easy_682_baseball_game.ts
+++ Easy/682_baseball_game/Easy_682_baseball_game.ts
@@ -1,25 +1,28 @@
 ﻿var calPoints = function (ops: string[]) {
     let sum = 0;
-    const arr: string[] = [];
+    const arr: number[] = [];
     let newValue = 0;
     ops.forEach(operation => {
         switch (operation) {
             case "+":
-                newValue = +arr[arr.length - 1] + +arr[arr.length - 2];
+                newValue = arr[arr.length - 1] + arr[arr.length - 2];
                 break;
             case "D":
-                newValue = +arr[arr.length - 1] * 2;
+                newValue = arr[arr.length - 1] * 2;
                 break;
             case "C":
-                newValue = -Number(arr.pop());
+                newValue = -arr[arr.length - 1];
+                arr.pop();
                 break;
             default:
-                newValue = +operation;
+                newValue = Number(operation);
                 break;
         }
         if (operation !== "C")
-            arr.push(newValue.toString());
+            arr.push(newValue);
         sum += newValue;
     });
     return sum;
 };
+
+const res = calPoints(["5", "2", "C", "D", "+"]);

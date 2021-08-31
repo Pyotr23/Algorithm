97baaceffc5489de11b791bfcd4b15694359cf7a
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-08-31 11:35:49 +0300

    Easy/easy 228 summary ranges (#15)
    * Создание проекта.
    
    * Первый тест успешен.
    
    * Дописаны тесты. Результат: 248 ms (54.62%), 30.8 MB (43.08%).
    
    * Минимальный по памяти вариант с массивом и двумя переменными.
    
    Co-authored-by: BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>

---
+++ Easy/SummaryRanges/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+
+namespace Easy.SummaryRanges
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+        }
+        
+        internal static IList<string> MyVersionSummaryRanges(int[] nums)
+        {
+            var list = new List<string>();
+            
+            switch (nums.Length)
+            {
+                case 0:
+                    return list;
+                case 1:
+                    list.Add(nums[0].ToString());
+                    return list;
+            }
+
+            var tempString = nums[0].ToString();
+            var next = nums[0] + 1;
+            var start = nums[0];
+            var current = 0;
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                current = nums[i];
+
+                if (next == current)
+                {
+                    next = current + 1;
+                    continue;
+                }
+
+                if (!(start == current ||  start == nums[i - 1]))
+                    tempString += $"->{nums[i - 1]}";
+
+                list.Add(tempString);
+
+                tempString = current.ToString();
+                start = current;
+                next = current + 1;
+            }
+            
+            if (start != current)
+                tempString += $"->{current}";
+
+            list.Add(tempString);
+
+            return list;
+        }
+        
+        internal static IList<string> SummaryRanges(int[] nums)
+        {
+            var summary = new List<string>();
+            
+            // i отвечает за начальное значение, j бежит по массиву
+            for (int i = 0, j = 0; j < nums.Length; j++) 
+            {
+                if (j + 1 < nums.Length && nums[j + 1] == nums[j] + 1)
+                    continue;
+                
+                if (i == j)
+                    summary.Add(nums[i].ToString());
+                else
+                    summary.Add(nums[i] + "->" + nums[j]);
+                
+                i = j + 1;
+            }
+            return summary;
+        }
+    }
+}
---
+++ Easy/SummaryRanges/Tests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Easy.SummaryRanges
+{
+    public class Tests
+    {
+        [Test]
+        public void FirstLongArray()
+        {
+            var arr = new[] {0, 1, 2, 4, 5, 7};
+
+            var list = Program.SummaryRanges(arr).ToList();
+            
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual("0->2", list.ElementAt(0));
+            Assert.AreEqual("4->5", list.ElementAt(1));
+            Assert.AreEqual("7", list.ElementAt(2));
+        }
+        
+        [Test]
+        public void SecondLongArray()
+        {
+            var arr = new[] {0, 2, 3, 4, 6, 8, 9};
+
+            var list = Program.SummaryRanges(arr).ToList();
+            
+            Assert.AreEqual(4, list.Count);
+            Assert.AreEqual("0", list.ElementAt(0));
+            Assert.AreEqual("2->4", list.ElementAt(1));
+            Assert.AreEqual("6", list.ElementAt(2));
+            Assert.AreEqual("8->9", list.ElementAt(3));
+        }
+        
+        [Test]
+        public void EmptyArray()
+        {
+            var arr = Array.Empty<int>();
+
+            var list = Program.SummaryRanges(arr).ToList();
+            
+            Assert.Zero(list.Count);
+        }
+        
+        [Test]
+        public void OneValue()
+        {
+            var arr = new[]{-1};
+
+            var list = Program.SummaryRanges(arr).ToList();
+            
+            Assert.AreEqual("-1", list.ElementAt(0));
+        }
+        
+        [Test]
+        public void OneValueIsZero()
+        {
+            var arr = new[] {0};
+
+            var list = Program.SummaryRanges(arr).ToList();
+            
+            Assert.AreEqual("0", list.ElementAt(0));
+        }
+    }
+}

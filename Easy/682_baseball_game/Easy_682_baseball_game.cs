5b5bcd1943dcd4e94ca246433dd9ea868e10d52f
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-08-31 08:58:38 +0300

    Easy/682 baseball game (#14)
    * Задание решено на C# (время - 66%, место - 8%).
    
    * Задание решено на JS (время - 65%, место - 26%).
    
    * Задание решено на JS (время - 83%, место - 26%).
    
    * Задание решено на JS (время - 27%, место - 18%).
    
    * Задание решено на TS (время - 64%, место - 27%).
    
    Co-authored-by: Pyotr Kolesnikov <Pyotr.Kolesnikov@indusoft.ru>

---
+++ Easy/682_baseball_game/Easy_682_baseball_game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Easy._682_baseball_game
+{
+    class Easy_682_baseball_game
+    {
+        //static void Main(string[] args)
+        //{
+        //    var arr = new string[] { "5", "-2", "4", "C", "D", "9", "+", "+" };
+        //    Console.WriteLine(CalPoints(arr));
+        //    Console.ReadKey();
+        //}
+
+        public static int CalPoints(string[] ops)
+        {
+            var sum = 0;
+            var stack = new Stack<int>();
+            int newValue;
+            foreach (var operation in ops)
+            {
+                switch (operation)
+                {
+                    case "+":
+                        int lastValue = stack.Pop();
+                        newValue = lastValue + stack.Peek();
+                        sum += newValue;
+                        stack.Push(lastValue);
+                        stack.Push(newValue);
+                        break;
+                    case "D":
+                        newValue = stack.Peek() * 2;
+                        stack.Push(newValue);
+                        sum += newValue;
+                        break;
+                    case "C":                        
+                        sum -= stack.Pop();
+                        break;
+                    default:
+                        newValue = int.Parse(operation);
+                        stack.Push(newValue);
+                        sum += newValue;
+                        break;
+                }
+            }
+            return sum;
+        }
+    }
+}

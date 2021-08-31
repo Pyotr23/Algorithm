666000caa6a78dbbe6975a2affed3e4617596f94
BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>
2021-08-31 12:13:40 +0300

    Подготовка к решению.

---
+++ Easy/CoveredIntegers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CoveredIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+        public bool IsCovered(int[][] ranges, int left, int right) {
+        
+        }
+    }
+}

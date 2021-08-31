ea3d75ed975846d8aaf6f852a1e341669e5d8375
BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>
2021-08-31 09:13:24 +0300

    Создание проекта.

---
+++ Easy/SummaryRanges/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Easy.SummaryRanges
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var nums = new[] { 0, 1, 2, 4, 5, 7 };
+        }
+        
+        public IList<string> SummaryRanges(int[] nums)
+        {
+            
+        }
+    }
+}

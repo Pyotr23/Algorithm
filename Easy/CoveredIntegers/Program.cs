fde25e07ebec2ca5c201a578112e6d453e553538
BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>
2021-09-01 08:31:21 +0300

    Рабочее решение на HashSet.

--- Easy/CoveredIntegers/Program.cs
+++ Easy/CoveredIntegers/Program.cs
@@ -1,13 +1,54 @@
-﻿namespace CoveredIntegers
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoveredIntegers
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var ranges = new[]
+            {
+                new[] {1, 2},
+                new[] {3, 4},
+                new[] {5, 6}
+            };
+            Console.WriteLine(IsCovered(ranges, 1, 6));
+
+            ranges = new[]
+            {
+                new[] {1, 10},
+                new[] {10, 20}
+            };
+            Console.WriteLine(IsCovered(ranges, 21, 21));
         }
         
-        public bool IsCovered(int[][] ranges, int left, int right) {
-        
+        public static bool IsCovered(int[][] ranges, int left, int right)
+        {
+            var hashSet = CreateHashSet(ranges);
+            
+            for (var i = left; i <= right; i++)
+            {
+                if (hashSet.Add(i))
+                    return false;;
+            }
+
+            return true;
+        }
+
+        private static HashSet<int> CreateHashSet(int[][] ranges)
+        {
+            var hashSet = new HashSet<int>();
+            
+            foreach (var range in ranges)
+            {
+                for (var i = range[0]; i <= range[1]; i++)
+                {
+                    hashSet.Add(i);
+                }
+            }
+
+            return hashSet;
         }
     }
 }

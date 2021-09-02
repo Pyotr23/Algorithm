1cde9dbe19c4a69fbea62064863c8f97e192890a
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-09-02 12:37:25 +0300

    Medium/medium elemental words (#17)
    * Подготовка проекта и словаря.
    
    * Получилось решить для слова "snack".
    
    * Конечный вариант с полями.
    
    * Убраны почти все поля. Рефакторинг.
    
    * Решение разбито по классам.
    
    * Убрано поле результирующего списка. Он передаётся параметром в методы.
    
    * Правки в словаре с элементами.
    
    Co-authored-by: BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>

---
+++ Medium/ElementalWords/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElementalWords
+{
+    internal static class Program
+    {
+        private static string _lowerCaseWord;
+        private static ElementCollection _elementCollection;
+
+        private static void Main()
+        {
+            ElementalForms("snack")
+                .ToList()
+                .ForEach(variant =>
+                {
+                    var str = StringHelper.GetJoinedViaCommaString(variant);
+                    Console.WriteLine(str);
+                });
+        }
+
+        private static IEnumerable<string[]> ElementalForms(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+                return Array.Empty<string[]>();
+            
+            _lowerCaseWord = word.ToLower();
+            _elementCollection = new ElementCollection();
+            var resultList = new List<List<string>>();
+
+            resultList = _elementCollection
+                .GetElementKeys(_lowerCaseWord)
+                .Aggregate(resultList, CheckElementKey());
+
+            return _elementCollection.GetElementalForms(resultList);
+        }
+
+        private static Func<List<List<string>>, string, List<List<string>>> CheckElementKey()
+        {
+            return (current, elementKey) 
+                => ElementalForms(new List<string> {elementKey}, current);
+        }
+
+        private static List<List<string>> ElementalForms(List<string> abbreviations, List<List<string>> resultList)
+        {
+            
+            if (_lowerCaseWord.Equals(StringHelper.GetJoinedString(abbreviations)))
+            {
+                resultList.Add(abbreviations);
+                return resultList;
+            }
+
+            var elementKeys = _elementCollection.GetElementKeys(_lowerCaseWord, abbreviations);
+            
+            foreach (var elementKey in elementKeys)
+            {
+                var enlargedAbbreviationList = new List<string>(abbreviations) {elementKey};
+                ElementalForms(enlargedAbbreviationList, resultList);
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ Medium/ElementalWords/StringHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ElementalWords
+{
+    internal static class StringHelper
+    {
+        internal static string GetJoinedString(IEnumerable<string> stringParts)
+        {
+            return string.Join("", stringParts);
+        }
+        
+        internal static string GetJoinedViaCommaString(IEnumerable<string> stringParts)
+        {
+            return string.Join(", ", stringParts);
+        }
+    }
+}

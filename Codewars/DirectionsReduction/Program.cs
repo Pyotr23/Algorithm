a74c96c9075a397860a13831dd1cc45a539df58b
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-09-09 16:46:10 +0300

    Codewars/5kyu directions reduction (#19)
    * Начало работы.
    
    * Неправильно понял задачу.
    
    * Работает со стеком.
    
    * Реализация на списке.
    
    * Реализация на двусвязном списке.
    
    Co-authored-by: BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>

---
+++ Codewars/DirectionsReduction/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars.Five.DirectionsReduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = dirReduc(new[] {"NORTH", "WEST", "SOUTH", "EAST"});
+            result = dirReduc(new[] {"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"});
+        }
+        
+        public static string[] dirReduc(string[] arr)
+        {
+            var directions = new Dictionary<string, string>
+            {
+                {"NORTH", "SOUTH"},
+                {"SOUTH", "NORTH"},
+                {"WEST", "EAST"},
+                {"EAST", "WEST"}
+            };
+
+            var list = new LinkedList<string>();
+            
+            foreach (var direction in arr)
+            {
+                if (list.Count == 0)
+                {
+                    
+                    list.AddLast(direction);
+                    continue;
+                }
+                
+                var oppositeDirection = directions[direction];
+                 
+                if (list.Last?.Value == oppositeDirection)
+                {
+                    list.RemoveLast();
+                    continue;
+                }
+                
+                list.AddLast(direction);
+            }
+
+            return list.ToArray();
+        }
+    }
+}

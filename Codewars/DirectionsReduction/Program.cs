5873263c39890aec0903309f875e00f83f4ac6e4
BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>
2021-09-09 12:36:03 +0300

    Начало работы.

---
+++ Codewars/DirectionsReduction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Codewars.Five.DirectionsReduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        public static string[] dirReduc(String[] arr) {
+            
+        }
+    }
+}

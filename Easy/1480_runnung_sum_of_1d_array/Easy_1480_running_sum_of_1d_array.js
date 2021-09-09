b544e7f3d0d0a6802671019e0a225a480cdf6776
BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>
2021-09-09 11:23:29 +0300

    Исправления в структуре решения.

--- Easy/1480_runnung_sum_of_1d_array/Easy_1480_running_sum_of_1d_array.js
+++
@@ -1,4 +0,0 @@
-﻿function runningSum(nums) {
-    let curr = 0;
-    return nums.map(num => curr += num);
-};
--- Easy/1480_runnung_sum_of_1d_array/Easy_1480_running_sum_of_1d_array.ts
+++
@@ -1,4 +0,0 @@
-﻿function runningSum(nums: number[]): number[] {
-    let curr = 0;
-    return nums.map(num => curr += num);
-};

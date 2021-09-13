e8e0f9706d967c3f63f76fe1aa3d56f610d47c17
Pyotr <2chilavert3@mail.ru>
2021-09-14 00:03:51 +0300

    Задача решена. Можно было использовать набор известный ключей.

--- Codewars/MissingKeyMadness/Program.cs
+++ Codewars/MissingKeyMadness/Program.cs
@@ -15,26 +15,46 @@ namespace Corewars.Five.MissingKeyMadness
         
         public static string SearchForKey(string[] messages, string[] secrets)
         {
-            var inputChars = messages
-                .SelectMany(m => m.ToCharArray())
-                .ToArray();
-            var outputChars = secrets
-                .SelectMany(s => s.ToCharArray())
-                .ToArray();
-            var hashSet = new HashSet<string>();
-            for (var i = 0; i < inputChars.Length; i++)
+            var messageVariants = GetPermutations(messages, messages.Length).ToArray();
+            var secret = string.Join("", secrets);
+            foreach (var message in messageVariants)
             {
-                if (inputChars[i] == outputChars[i])
+                var secretWord = SearchForKey(message, secret);
+                if (secretWord.Length == 12)
+                    return secretWord;
+            }
+
+            return string.Empty;
+        }
+        
+        private static string SearchForKey(string message, string secret)
+        {
+            var hashSet = new HashSet<string>();
+            
+            for (var i = 0; i < message.Length; i++)
+            {
+                if (message[i] == secret[i])
                     continue;
 
-                var key = (char)Math.Min(inputChars[i], outputChars[i]);
-                var value = (char) Math.Max(inputChars[i], outputChars[i]);
+                var key = (char)Math.Min(message[i], secret[i]);
+                var value = (char) Math.Max(message[i], secret[i]);
                 var str = string.Join("", key, value);
                 if (!hashSet.Contains(str))
                     hashSet.Add(str);
             }
 
             return string.Join("", hashSet.OrderBy(x => x).ToArray());
+        }
+        
+        private static IEnumerable<string> GetPermutations(IEnumerable<string> list, int length)
+        {
+            if (length == 1) 
+                return list;
+        
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(e => !t.Contains(e)),
+                    (t1, t2) => t1.Concat(t2))
+                .Select(x => string.Join("", x));
         }
     }
 }

64fb2877a7aa04a1c5b30a25aeb940e9b03e535c
Pyotr <2chilavert3@mail.ru>
2021-09-15 00:08:46 +0300

    Лучшее решение на мой вкус.

--- Codewars/LastDigitSymmetry/Program.cs
+++ Codewars/LastDigitSymmetry/Program.cs
@@ -1,84 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 
 namespace Codewars.Six.LastDigitSymmetry
 {
-    class Program
+    internal static class Program
     {
-        private static readonly HashSet<int> _primeDigits = new HashSet<int>();
-        
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine(Solve(1, 1200));
         }
 
-        public static int Solve(int min, int max)
+        private static int Solve(int min, int max) 
+            => Enumerable
+                .Range(min,max-min)
+                .Count(x => Satisfies(x));
+        
+        private static bool Satisfies(long x) 
+            => LastTwo(x*x) == LastTwo(x) 
+               && IsTwoDigitPrime(FirstTwo(x)) 
+               && IsTwoDigitPrime(FirstTwo(x*x));
+
+        private static long FirstTwo(long x)
         {
-            var count = max - min + 1;
-            var enumerable = Enumerable.Range(min, count);
-            FillPrimeDigits();
-            var result = 0;
-            foreach (var digit in enumerable)
-            {
-                var stringDigit = digit.ToString();
-                var stringSquare = Math.Pow(digit, 2).ToString(CultureInfo.InvariantCulture);
-                if (IsSameLastTwoDigit(stringDigit, stringSquare)
-                    && IsPrimeFirstTwoDigit(stringDigit)
-                    && IsPrimeFirstTwoDigit(stringSquare)
-                    )
-                    result++;
-            }
-
-            return result;
-        }
-
-        private static void FillPrimeDigits()
-        {
-            var enumerable = Enumerable.Range(11, 99);
-            foreach (var digit in enumerable)
-            {
-                if (IsPrime(digit))
-                    _primeDigits.Add(digit);
-            }
-        }
-
-        private static bool IsPrimeFirstTwoDigit(string value)
-        {
-            if (value.Length < 2)
-                return false;
-           
-            var number = int.Parse(value[..2]);
-
-            return _primeDigits.Contains(number);
-        }
-
-        private static bool IsSameLastTwoDigit(string value, string square)
-        {
-            if (value.Length < 2)
-                return false;
-            var valueInteger = int.Parse(value[^2..]);
-            var squareInteger = int.Parse(square[^2..]);
-            return valueInteger == squareInteger;
+            while (x >= 100) 
+                x /= 10;
+            return x;
         }
         
-        private static bool IsPrime(int numb)
-        {
-            if (!(numb % 2 == 0 || numb % 3 == 0 || numb % 5 == 0))
-            { 
-                for (var i = 3; i < numb / 3; i += 2)
-                {
-                    if ((double) numb % i != 0) 
-                        continue;
-                    
-                    return false;
-                }
-            }
-            else
-                return false;
-            
-            return true;
-        }
+        private static long LastTwo(long x) => x % 100;
+        
+        private static bool IsTwoDigitPrime(long x) 
+            =>  !(x <= 10 
+                  || x % 2 == 0 
+                  || x % 3 == 0 
+                  || x % 5 == 0 
+                  || x % 7 == 0); 
     }
 }

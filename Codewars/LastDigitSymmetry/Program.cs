75af2a83c47ca37c9bc01ec226d25d43f7d009ff
Pyotr <2chilavert3@mail.ru>
2021-09-14 23:04:14 +0300

    Не проходит финальный тест по времени.

--- Codewars/LastDigitSymmetry/Program.cs
+++ Codewars/LastDigitSymmetry/Program.cs
@@ -7,6 +7,8 @@ namespace Codewars.Six.LastDigitSymmetry
 {
     class Program
     {
+        private static readonly HashSet<int> _primeDigits = new HashSet<int>();
+        
         static void Main(string[] args)
         {
             Console.WriteLine(Solve(1, 1200));
@@ -16,42 +18,53 @@ namespace Codewars.Six.LastDigitSymmetry
         {
             var count = max - min + 1;
             var enumerable = Enumerable.Range(min, count);
+            FillPrimeDigits();
             var result = 0;
             foreach (var digit in enumerable)
             {
                 var stringDigit = digit.ToString();
                 var stringSquare = Math.Pow(digit, 2).ToString(CultureInfo.InvariantCulture);
-                if (IsPrimeFirstTwoDigit(stringDigit)
+                if (IsSameLastTwoDigit(stringDigit, stringSquare)
+                    && IsPrimeFirstTwoDigit(stringDigit)
                     && IsPrimeFirstTwoDigit(stringSquare)
-                    && IsSameLastTwoDigit(stringDigit, stringSquare))
+                    )
                     result++;
             }
 
             return result;
         }
 
+        private static void FillPrimeDigits()
+        {
+            var enumerable = Enumerable.Range(11, 99);
+            foreach (var digit in enumerable)
+            {
+                if (IsPrime(digit))
+                    _primeDigits.Add(digit);
+            }
+        }
+
         private static bool IsPrimeFirstTwoDigit(string value)
         {
             if (value.Length < 2)
                 return false;
-            var number = int.Parse(string.Join("", value[..2]));
-            return IsPrime(number);
+           
+            var number = int.Parse(value[..2]);
+
+            return _primeDigits.Contains(number);
         }
 
         private static bool IsSameLastTwoDigit(string value, string square)
         {
             if (value.Length < 2)
                 return false;
-            var valueInteger = int.Parse(string.Join("", value[^2..]));
-            var squareInteger = int.Parse(string.Join("", square[^2..]));
+            var valueInteger = int.Parse(value[^2..]);
+            var squareInteger = int.Parse(square[^2..]);
             return valueInteger == squareInteger;
         }
         
         private static bool IsPrime(int numb)
         {
-            if (numb is 3 or 5)
-                return true;
-            
             if (!(numb % 2 == 0 || numb % 3 == 0 || numb % 5 == 0))
             { 
                 for (var i = 3; i < numb / 3; i += 2)

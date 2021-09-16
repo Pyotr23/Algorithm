c18825aa1d4a8a2fa4600017f0a63e376f82c591
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-09-16 23:51:06 +0300

    Codewars/5kyu moving zeroes to end (#23)
    * Решена задача с дополнительной памятью под временную переменную.
    
    * Лаконичное решение с дополнительным массивом.

---
+++ Codewars/MovingZeroesToEnd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Corewars.Five.MovingZeroesToEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                string.Join("--", 
+                    MoveZeroes(new[] { 0, 0, 0, 0, 3 }))); 
+        }
+        
+        public static int[] MoveZeroes(int[] arr)
+        {
+            if (arr.Length is 0 or 1)
+                return arr;
+
+            var result = new int[arr.Length];
+            
+            for (int i = 0, j = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                    continue;
+
+                result[j] = arr[i];
+                j++;
+            }
+
+            return result;
+        }
+    }
+}

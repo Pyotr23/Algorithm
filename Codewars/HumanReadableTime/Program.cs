9613ee5612d1a8745d1bde8d53fe771ac374b765
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-09-20 23:52:09 +0300

    Задача решена. Очередное изменение структуры решения. (#28)

---
+++ Codewars/HumanReadableTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Codewars.Five.HumanReadableTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetReadableTime(0));
+        }
+        
+        public static string GetReadableTime(int seconds)
+        {
+            var timespan = TimeSpan.FromSeconds(seconds);
+            var result = timespan.ToString(@"hh\:mm\:ss");
+            
+            if (timespan.Days == 0)
+                return result;
+            
+            var hours = timespan.Days * 24 + timespan.Hours;
+            return result
+                .Remove(0, 2)
+                .Insert(0, hours.ToString());
+        }
+    }
+}

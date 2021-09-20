e501f9ae036abcd67ba81f498ea3c3ec43c339e6
Pyotr <2chilavert3@mail.ru>
2021-09-21 00:06:15 +0300

    Решение с помощью стека.

---
+++ Codewars/ValidParentheses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codewars.Five.ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidParentheses("(())((()())())"));
+        }
+        
+        public static bool ValidParentheses(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return true;
+
+            var stack = new Stack<char>();
+
+            foreach (var letter in input)
+            {
+                switch (letter)
+                {
+                    case '(':
+                        stack.Push(letter);
+                        break;
+                    case ')':
+                        if (stack.TryPop(out var lastParentheses) || lastParentheses == '(')
+                            break;
+                        return false;
+                }
+            }
+            
+            return stack.Count == 0;
+        }
+    }
+}

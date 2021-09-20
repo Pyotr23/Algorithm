25f4a064fe8de5de945c2b865834f3cde50421b6
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-09-21 00:11:44 +0300

    Codewars/5kyu valid parentheses (#29)
    * Решение с помощью стека.
    
    * Небольшой рефакторинг.
    
    * Решение с помощью переменной-счётчика.

---
+++ Codewars/ValidParentheses/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Codewars.Five.ValidParentheses
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(ValidParentheses("(())((()())())"));
+        }
+
+        private static bool ValidParentheses(string input)
+        {
+            var count = 0;
+            foreach (var letter in input)
+            {
+                switch (letter)
+                {
+                    case '(':
+                        count++;
+                        break;
+                    case ')':
+                    {
+                        count--;
+                        if (count < 0)
+                            return false;
+                        break;
+                    }
+                }
+            }
+            
+            return count == 0;
+        }
+    }
+}

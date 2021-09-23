135060b013474ee7092b1fed054c056b3c57207b
Pyotr <2chilavert3@mail.ru>
2021-09-23 23:43:50 +0300

    Задача решена с помощью двух переменных и списка строк.

---
+++ Codewars/RangeExtraction/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codewars.Four.RangeExtraction
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(
+                Extract(new[] {-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20, 22}));
+            Console.WriteLine(
+                Extract(new[] { -3, -2, -1, 2, 10, 15, 16, 18, 19, 20 }));
+        }
+
+        private static string Extract(int[] args)
+        {
+            switch (args.Length)
+            {
+                case 0:
+                    return string.Empty;
+                case 1:
+                    return args[0].ToString();
+            }
+
+            var result = new List<string>(args.Length);
+            var start = args[0];
+            var end = args[0];
+            
+            for (var i = 1; i < args.Length; i++)
+            {
+                if (args[i] == args[i - 1] + 1)
+                {
+                    end = args[i];
+                    continue;
+                }
+                
+                Write(result, start, end);
+
+                start = args[i];
+                end = args[i];
+            }
+
+            Write(result, start, end);
+            
+            return string.Join(",", result);  
+        }
+
+        private static void Write(List<string> result, int start, int end)
+        {
+            if (start == end)
+                result.Add(start.ToString());
+            else if (start == end - 1)
+                result.AddRange(new []{start.ToString(), end.ToString()});
+            else
+                result.Add($"{start.ToString()}-{end.ToString()}");
+        }
+    }
+}

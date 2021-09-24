498f3cd15f1a3b1f0474969c704e2691ebe068f1
Pyotr <2chilavert3@mail.ru>
2021-09-25 00:48:03 +0300

    Задача решена.

--- Codewars/StringsMix/Program.cs
+++ Codewars/StringsMix/Program.cs
@@ -1,17 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Codewars.Four.StringsMix
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            const string first = "A generation must confront the looming ";
+            const string second = "codewarrs";
+            Console.WriteLine(Mix(first, second));
         }
-        
-        public static string Mix(string s1, string s2)
+
+        private static string Mix(string s1, string s2)
         {
-            // your code
+            var dictionary = new Dictionary<char, string[]>();
+            
+            foreach (var letter in s1.Where(l => char.IsLetter(l) && char.IsLower(l)))
+            {
+                if (dictionary.ContainsKey(letter))
+                {
+                    dictionary[letter][0] = dictionary[letter][0] + letter;
+                    continue;
+                }
+                dictionary.Add(letter, new[]{ letter.ToString(), string.Empty });
+            }
+            
+            foreach (var letter in s2.Where(l => char.IsLetter(l) && char.IsLower(l)))
+            {
+                if (dictionary.ContainsKey(letter))
+                {
+                    dictionary[letter][1] = dictionary[letter][1] + letter;
+                    continue;
+                }
+                dictionary.Add(letter, new[]{ string.Empty, letter.ToString() });
+            }
+
+            var result = dictionary
+                .Where(pair => pair.Value[0].Length > 1 || pair.Value[1].Length > 1)
+                .Select(pair =>
+                {
+                    var (_, value) = pair;
+                    var firstMax = value[0].Length;
+                    var secondMax = value[1].Length;
+                    
+                    if (firstMax > secondMax)
+                        return $"1:{value[0]}";
+                    if (secondMax > firstMax)
+                        return $"2:{value[1]}";
+                    return $"=:{value[0]}";
+                })
+                .OrderByDescending(x => x.Length)
+                .ThenBy(y =>
+                {
+                    return y[0] switch
+                    {
+                        '1' => 0,
+                        '2' => 1,
+                        _ => 2
+                    };
+                })
+                .ThenBy(z => z);
+            
+            return string.Join('/', result);
         }
     }
 }

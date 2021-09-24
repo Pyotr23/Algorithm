e74d33c19707e43afb85b4c9cea7212b2d46f68d
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-09-24 18:22:39 +0300

    Решение с помощью LINQ. (#33)
    Co-authored-by: BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>

---
+++ Codewars/CommentsStripper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars.Four.CommentsStripper
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(
+            StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", 
+                new[] { "#", "!" }));
+        }
+
+        private static string StripComments(string text, IEnumerable<string> commentSymbols)
+        {
+            var rows = text
+                .Split("\n")
+                .Select(str => string.Concat(str
+                    .TakeWhile(s => !commentSymbols.Contains(s.ToString())))
+                    .TrimEnd());
+            return string.Join("\n", rows);
+        }
+
+    }
+}

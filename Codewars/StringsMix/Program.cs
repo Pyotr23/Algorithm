324855afaefcb66fed2af9be74fc2dd6f703c10f
Pyotr <2chilavert3@mail.ru>
2021-09-24 21:44:34 +0300

    Подготовка к решению задачи.

---
+++ Codewars/StringsMix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Codewars.Four.StringsMix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        public static string Mix(string s1, string s2)
+        {
+            // your code
+        }
+    }
+}

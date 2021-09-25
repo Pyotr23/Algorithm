0897be8bbc60d094b3bcaf02632ed7b151776a6d
Pyotr <2chilavert3@mail.ru>
2021-09-25 12:27:30 +0300

    Подготовка к работе.

---
+++ Codewars/Permutations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codewars.Four.Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        public static List<string> SinglePermutations(string s)
+        {
+            // Your code here!
+            return null;
+        }
+    }
+}

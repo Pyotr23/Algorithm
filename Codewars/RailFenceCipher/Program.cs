b7649036810e61f07d75b5e2be77ad054601ba46
Pyotr <2chilavert3@mail.ru>
2021-09-25 21:34:46 +0300

    Подготовка проекта.

---
+++ Codewars/RailFenceCipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Codewars.Three.RailFenceCipher
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        public static string Encode(string s, int n)
+        {
+            // Your code here
+            return null;
+        }
+
+        public static string Decode(string s, int n)
+        {
+            // Your code here
+            return null;
+        }
+    }
+}

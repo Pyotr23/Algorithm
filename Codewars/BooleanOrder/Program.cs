aa224f462d0638ac392d95cf02ca44ecd5d6f5db
Pyotr <2chilavert3@mail.ru>
2021-09-25 23:03:50 +0300

    Подготовка кода для решения.

---
+++ Codewars/BooleanOrder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace Codewars.Three.BooleanOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public class BooleanOrder
+    {
+        public BooleanOrder(string operands, string operators)
+        {
+            // Your code here
+        }
+
+        public BigInteger Solve()
+        {
+            // Your code here
+            return BigInteger.Zero;
+        }
+    }
+}

56bf368124dcf977718258d9fe7679ee3781252e
Pyotr <2chilavert3@mail.ru>
2021-09-25 14:41:55 +0300

    Конечный результат.

--- Codewars/Permutations/Program.cs
+++ Codewars/Permutations/Program.cs
@@ -1,37 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Codewars.Four.Permutations
 {
     class Program
     {
-        private static HashSet<string> _hashSet = new HashSet<string>();
         static void Main(string[] args)
         {
-            var list = SinglePermutations("aabb");
+            SinglePermutations("aabb").ForEach(Console.WriteLine);
         }
         
         public static List<string> SinglePermutations(string s)
         {
             var hashSet = new HashSet<string>();
-            Permute(s, 0, s.Length - 1);
-            return null;
+            Permute(hashSet, s, 0, s.Length - 1);
+            return hashSet.ToList();
         }
         
-        private static void Permute(string str, int leftIndex, int rightIndex)
+        private static void Permute(HashSet<string> hashSet, 
+            string str, 
+            int leftIndex, 
+            int rightIndex)
         {
             if (leftIndex != rightIndex)
             {
                 for (var i = leftIndex; i <= rightIndex; i++)
                 {
                     str = Swap(str, leftIndex, i);
-                    Permute(str, leftIndex + 1, rightIndex);
+                    Permute(hashSet, str, leftIndex + 1, rightIndex);
                     str = Swap(str, leftIndex, i);
                 }
             }
 
-            if (!_hashSet.Contains(str))
-                _hashSet.Add(str);
+            if (!hashSet.Contains(str))
+                hashSet.Add(str);
         }
 
         private  static string Swap(string str, int leftIndex, int rightIndex)

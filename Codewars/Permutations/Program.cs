d2762ccac4bc4e62d7024f071b4775b97687f911
Pyotr <2chilavert3@mail.ru>
2021-09-25 14:49:39 +0300

    Лучший результат с помощью рекурсии, SelectMany  и поддиапазонов.

--- Codewars/Permutations/Program.cs
+++ Codewars/Permutations/Program.cs
@@ -4,45 +4,21 @@ using System.Linq;
 
 namespace Codewars.Four.Permutations
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
             SinglePermutations("aabb").ForEach(Console.WriteLine);
         }
-        
-        public static List<string> SinglePermutations(string s)
-        {
-            var hashSet = new HashSet<string>();
-            Permute(hashSet, s, 0, s.Length - 1);
-            return hashSet.ToList();
-        }
-        
-        private static void Permute(HashSet<string> hashSet, 
-            string str, 
-            int leftIndex, 
-            int rightIndex)
-        {
-            if (leftIndex != rightIndex)
-            {
-                for (var i = leftIndex; i <= rightIndex; i++)
-                {
-                    str = Swap(str, leftIndex, i);
-                    Permute(hashSet, str, leftIndex + 1, rightIndex);
-                    str = Swap(str, leftIndex, i);
-                }
-            }
 
-            if (!hashSet.Contains(str))
-                hashSet.Add(str);
-        }
-
-        private  static string Swap(string str, int leftIndex, int rightIndex)
-        {
-            var charArray = str.ToCharArray();
-            (charArray[leftIndex], charArray[rightIndex]) = (charArray[rightIndex], charArray[leftIndex]);
-            var result = new string(charArray);
-            return result;
-        }
+        private static List<string> SinglePermutations(string s) 
+            => s.Length < 2 
+                ? new List<string> { s } 
+                : SinglePermutations(s[1..])
+                    .SelectMany(x => Enumerable
+                        .Range(0, x.Length + 1)
+                        .Select((_, i) => x[..i] + s[0] + x[i..]))
+                .Distinct()
+                .ToList();
     }
 }

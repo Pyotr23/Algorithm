5fc2a5040fdad87e3ee7787e3e168435eef5640a
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-09-25 15:20:41 +0300

    Codewars/4kyu permutations (#35)
    * Подготовка к работе.
    
    * Черновой вариант с рекурсией.
    
    * Конечный результат.
    
    * Лучший результат с помощью рекурсии, SelectMany  и поддиапазонов.

---
+++ Codewars/Permutations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars.Four.Permutations
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            SinglePermutations("aabb").ForEach(Console.WriteLine);
+        }
+
+        private static List<string> SinglePermutations(string s) 
+            => s.Length < 2 
+                ? new List<string> { s } 
+                : SinglePermutations(s[1..])
+                    .SelectMany(x => Enumerable
+                        .Range(0, x.Length + 1)
+                        .Select((_, i) => x[..i] + s[0] + x[i..]))
+                .Distinct()
+                .ToList();
+    }
+}

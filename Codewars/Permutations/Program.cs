bd217dcbf57e71b560c75e8510afe42c55bac7b9
Pyotr <2chilavert3@mail.ru>
2021-09-25 14:37:13 +0300

    Черновой вариант с рекурсией.

--- Codewars/Permutations/Program.cs
+++ Codewars/Permutations/Program.cs
@@ -5,15 +5,41 @@ namespace Codewars.Four.Permutations
 {
     class Program
     {
+        private static HashSet<string> _hashSet = new HashSet<string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var list = SinglePermutations("aabb");
         }
         
         public static List<string> SinglePermutations(string s)
         {
-            // Your code here!
+            var hashSet = new HashSet<string>();
+            Permute(s, 0, s.Length - 1);
             return null;
+        }
+        
+        private static void Permute(string str, int leftIndex, int rightIndex)
+        {
+            if (leftIndex != rightIndex)
+            {
+                for (var i = leftIndex; i <= rightIndex; i++)
+                {
+                    str = Swap(str, leftIndex, i);
+                    Permute(str, leftIndex + 1, rightIndex);
+                    str = Swap(str, leftIndex, i);
+                }
+            }
+
+            if (!_hashSet.Contains(str))
+                _hashSet.Add(str);
+        }
+
+        private  static string Swap(string str, int leftIndex, int rightIndex)
+        {
+            var charArray = str.ToCharArray();
+            (charArray[leftIndex], charArray[rightIndex]) = (charArray[rightIndex], charArray[leftIndex]);
+            var result = new string(charArray);
+            return result;
         }
     }
 }

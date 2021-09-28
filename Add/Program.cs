021bb9355cfc4901dd56087c39f58c0ced511b81
BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>
2021-09-28 17:16:52 +0300

    Если число не меньше 990, то его можно представить.

--- Add/Program.cs
+++ Add/Program.cs
@@ -1,15 +1,47 @@
-﻿namespace Route256.OneStar.Add
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Route256.OneStar.Add
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var setsCount = int.Parse(args[0]);
+            // var setsCount = int.Parse(args[0]);
+            // if ()
+            Fill();
         }
 
-        private static void IsPossibleToGet(int number)
+        // private static bool IsPossibleToGet(int number)
+        // {
+        //     for (var i = 0; i < 10; i++)
+        //     {
+        //         
+        //     }
+        //     var res = number / 111;
+        //     if (res >= 10)
+        //         return true;
+        //     
+        //     if (res )
+        // } 
+
+        private static void Fill()
         {
-            
-        } 
+            var set = new HashSet<int>(1000);
+            for (var i = 0; i < 10; i++)
+            {
+                var j = 0;
+                var res = 0;
+                while (res < 1000)
+                {
+                    res = i * 111 + j * 11;
+                    if (!set.Contains(res))
+                        set.Add(res);
+                    j++;
+                }
+            }
+
+            var arr = set.OrderBy(x => x).ToArray();
+        }
     }
 }

67fc693fb58e0a98b947b8599d66e61617ce58ed
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-09-28 22:57:52 +0300

    Решено. (#40)

---
+++ Route256/Boxes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Boxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var boxCount = int.Parse(Console.ReadLine());
+            var numbers = Console
+                .ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .ToArray();
+            
+            var result = 0;
+            for (var i = 0; i < boxCount; i += 2)
+            {
+                result += numbers[i + 1] - numbers[i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

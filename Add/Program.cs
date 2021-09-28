73fe0a8688377509eac47ced73cd2b5c8b5bcf47
BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>
2021-09-28 15:13:55 +0300

    Создан проект.

---
+++ Add/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Route256.OneStar.Add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setsCount = int.Parse(args[0]);
+        }
+
+        private static void IsPossibleToGet(int number)
+        {
+            
+        } 
+    }
+}

30f74936b72cb17854fe2849d60d231e50e34803
Pyotr <2chilavert3@mail.ru>
2021-09-28 23:54:56 +0300

    Правильно нахождение суммы. Осталось вывести дробь.

---
+++ Route256/GaltonCasino/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Route256.ThreeStars.GaltonCasino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setCount = int.Parse(Console.ReadLine());
+            for (var i = 0; i < setCount; i++)
+            {
+                var depth = int.Parse(Console.ReadLine());
+                var result = 0;
+                for (var j = 0; j < depth; j++)
+                {
+                    var count = (int)Math.Pow(2, depth - j - 1);
+                    result += Console
+                        .ReadLine()
+                        .Split(' ')
+                        .Select((x, ind) => IsOuterHex(j, ind)
+                            ? int.Parse(x) * count
+                            : int.Parse(x) * count * 2)
+                        .Sum();
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+
+        private static bool IsOuterHex(int rowIndex, int hexIndex)
+        {
+            return rowIndex < 2 
+                   || hexIndex == 0 
+                   || hexIndex == rowIndex;
+        }
+    }
+}

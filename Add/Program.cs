6f56a572b81e0aa625ed77cffe8f8bbcd3f57077
Pyotr23 <35201710+Pyotr23@users.noreply.github.com>
2021-09-28 21:21:23 +0300

    Route256/one star summands (#38)
    * Создан проект.
    
    * Если число не меньше 990, то его можно представить.
    
    * Хорошее решение.
    
    Co-authored-by: BMOFFICE\p.kolesnikov <p.kolesnikov@finflowsoft.com>

---
+++ Add/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Route256.OneStar.Add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setsCount = int.Parse(Console.ReadLine());
+            for (var i = 0; i < setsCount; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+                var res = IsPossibleToGet(number)
+                    ? "YES"
+                    : "NO";
+                Console.WriteLine(res);
+            }
+        }
+
+        private static bool IsPossibleToGet(int number)
+        {
+            if (number >= 990)
+                return true;
+
+            for (var i = number / 111 * 111; i >= 0; i -= 111)
+            {
+                var current = (number - i) % 11;
+                if (current == 0)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}

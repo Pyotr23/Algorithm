9bad2a697e425c92fcc0e6a4867ae4870ea9613c
Pyotr <2chilavert3@mail.ru>
2021-09-28 21:48:33 +0300

    Решено.

---
+++ SkirtingBoard/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Route256.OneStar.SkirtingBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console
+                .ReadLine()
+                .Split(' ');
+            var maxLength = int.Parse(inputs[0]);
+            var totalLength = int.Parse(inputs[1]);
+            var res = totalLength % maxLength == 0
+                ? totalLength / maxLength
+                : totalLength / maxLength + 1;
+            Console.WriteLine(res);
+        }
+    }
+}

168edd8524775ccd735d8f362a39e68c40cca9c6
Pyotr <2chilavert3@mail.ru>
2021-09-30 23:37:03 +0300

    Рабочая простыня. Сначала проверка на лишний закрывающий тег, затем - на лишний открывающий.

--- Route256/BrokenHtml/Program.cs
+++ Route256/BrokenHtml/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
 namespace Route256.FourStar.BrokenHtml
 {
@@ -13,21 +12,27 @@ namespace Route256.FourStar.BrokenHtml
             // var queue = new Queue<string>(new string[] { "1", "6", "<HTML>", "<TAG>", "<button>", "</BUTTON>", "<TAG>", "</HTML>" });
             // var queue = new Queue<string>(new string[] { "1", "1", "<HTML>" });
             // var queue = new Queue<string>(new string[] { "1", "5", "</KUKA>", "<HTML>", "<biba>", "</BIBA>", "</HTML>" });
-            var queue = new Queue<string>(new string[] { "1", "5", "<X>", "<Y>", "</X>", "</Y>", "</X>" });
+            // var queue = new Queue<string>(new string[] { "1", "5", "<X>", "<Y>", "<X>", "</Y>", "</X>" });
 
-            var setCount = int.Parse(queue.Dequeue());
+            var setCount = int.Parse(Console.ReadLine());
             for (var i = 0; i < setCount; i++)
             {
-                var tagCount = int.Parse(queue.Dequeue());
+                var tagCount = int.Parse(Console.ReadLine());
                 var stack = new Stack<string>();
                 var errorCount = 0;
                 var errorTag = string.Empty;
-                for (var j = 0; j < tagCount; j++)
+
+                var tags = new Queue<string>();
+                while (tagCount != 0)
+                {
+                    tags.Enqueue(Console.ReadLine().ToUpper());
+                    tagCount--;
+                }
+
+                foreach (var tag in tags)
                 {
                     if (errorCount > 1)
                         break;
-                    
-                    var tag = queue.Dequeue().ToUpper();
 
                     if (!tag.StartsWith("</"))
                     {
@@ -48,20 +53,67 @@ namespace Route256.FourStar.BrokenHtml
                     if (lastTag == tag.Remove(1, 1))
                         continue;
 
+                    errorTag = tag;
+                    stack.Push(lastTag);
                     errorCount++;
+                }
+
+                if (stack.Count == 0 && errorCount == 0)
+                {
+                    Console.WriteLine("CORRECT");
+                    continue;
+                }
+                 
+                if (stack.Count + errorCount == 1)
+                {
+                    if (stack.Count == 1)
+                        errorTag = stack.Pop();
+                    Console.WriteLine("ALMOST " + errorTag);
+                    continue;
+                }
+                
+                stack = new Stack<string>();
+                errorCount = 0;
+                errorTag = string.Empty;
+                
+                foreach (var tag in tags)
+                {
+                    if (errorCount > 1)
+                        break;
+
+                    if (!tag.StartsWith("</"))
+                    {
+                        stack.Push(tag);
+                        continue;
+                    }
+
+                    string lastTag;
+                    if (!stack.TryPeek(out lastTag))
+                    {
+                        errorTag = tag;
+                        errorCount++;
+                        continue;
+                    }
                     
-                    string penultimateOpenedTag;
-                    if (stack.TryPeek(out penultimateOpenedTag) && penultimateOpenedTag == tag.Remove(1, 1))
+                    stack.Pop();
+
+                    if (lastTag == tag.Remove(1, 1))
+                        continue;
+                    
+                    errorCount++;
+
+                    string previousTag;
+                    if (stack.TryPeek(out previousTag) && previousTag == tag.Remove(1, 1))
                     {
                         errorTag = lastTag;
                         stack.Pop();
                         continue;
                     }
-                    
-                    stack.Push(lastTag);
-                    errorTag = tag;
-                }
 
+                    errorCount++;
+                    break;
+                }
+                
                 if (stack.Count == 0 && errorCount == 0)
                 {
                     Console.WriteLine("CORRECT");
